50ba675ca175aaf5f92195e8566603b2af9bbeec
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-09-11 00:03:23 +0800

    20250911 0003

--- pandas/test.py
+++ pandas/test.py
@@ -1,13 +1,46 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv(
-    "X:\\学习\\研究生\\代码数据库\\Learning\\pandas\\pandas_for_everyone-master\\data\\gapminder.tsv",
-    sep="\t",
-)
+# df = pd.read_csv(
+#     "Learning/pandas/pandas_for_everyone-master/data/gapminder.tsv",
+#     sep="\t",
+# )
+
+# print(df.head(5))
+# # print(df.info)
+# res = df.groupby(["year", "continent"])[["lifeExp", "gdpPercap"]].mean()
+# # res = df.groupby(["continent"]).value_counts(["pop"])
+# res.plot()
+# plt.show()
 
 
-print(df.head(5))
-# print(df.info)
+# data = pd.DataFrame(
+#     {"name": ["A", "B"], "age": [12, 34], "gender": ["male", "female"]},
+#     index=[
+#         "one",
+#         "two",
+#     ],
+#     columns=["name", "gender", "age"],
+# )
 
-res = df.groupby(["year", "continent"])[["lifeExp", "gdpPercap"]].mean()
-print(res)
+# print(df.head())
+# res = df.groupby(["year"])["lifeExp"].mean()
+
+# print(res)
+
+# res.plot()
+# plt.xlabel("year")
+# plt.ylabel("pop")
+# plt.show()
+
+
+df = pd.read_csv("Learning/pandas/pandas_for_everyone-master/data/scientists.csv")
+
+ages = df["Age"]
+print(df.head())
+
+print(ages)
+print(pd.Series([1, 100]))
+
+print(pd.Series([1, 100]) + ages)
